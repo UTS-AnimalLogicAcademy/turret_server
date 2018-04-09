ead4d4304fe567faff63e04b02c1209f5b5bf044
Benjamin Skinner <benjamin.skinner@student.uts.edu.au>
2018-04-09 10:43:57 +1000

    Extracted zmq_server from uri_resolver rez package and git repo

---
+++ package.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+name = 'zmq_server'
+
+version = '0.0.1'
+
+authors = ['wen.tan',
+           'ben.skinner']
+
+tools = ['start_uri_server']
+
+requires = ['pyzmq',
+            'uri_resolver']
+
+build_requires = ['python']
+
+def commands():
+    env.PYTHONPATH.append('{root}/python')
+    env.ZMQ_SERVER = '{root}/python'
+    env.PATH.append('{root}/bin')
+    
+uuid = '3f17be78-3dd3-4a36-879c-0cf898e088ea'
+
