754626362b08509ef0a89ff75b82ac37957c4a41
Benjamin Skinner <benjamin.skinner@student.uts.edu.au>
2018-04-09 10:45:04 +1000

    Removed zmq server and dependencies

--- package.py
+++ package.py
@@ -1,22 +1,15 @@
 # -*- coding: utf-8 -*-
 name = 'uri_resolver'
 
-version = '0.0.10'
+version = '0.0.11'
 
 authors = ['daniel.flood',
            'ben.skinner']
 
-tools = ['start_uri_server']
-
-requires = ['pyzmq']
-
 build_requires = ['python']
 
 def commands():
-    env.PYTHONPATH.append('{root}/python')
-    env.URI_SERVER = '{root}/python/uri_zmq'
-    env.PATH.append('{root}/bin')
-    
+    env.PYTHONPATH.append('{root}/python')    
 
 uuid = '521166cd-725a-402c-a424-e68d24c448fc'
 
