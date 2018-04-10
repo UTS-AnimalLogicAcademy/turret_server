47acf9692ecc67dbc6e3be25bfd1ef3eeb7440ef
Wen Tan <13184268@koala.ala.int.uts.edu.au>
2018-04-10 10:00:32 +1000

    version up

--- package.py
+++ package.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 name = 'zmq_server'
 
-version = '0.0.1'
+version = '0.0.2'
 
 authors = ['wen.tan',
            'ben.skinner']
@@ -17,6 +17,5 @@ def commands():
     env.PYTHONPATH.append('{root}/python')
     env.ZMQ_SERVER = '{root}/python'
     env.PATH.append('{root}/bin')
-    
-uuid = '3f17be78-3dd3-4a36-879c-0cf898e088ea'
 
+uuid = '3f17be78-3dd3-4a36-879c-0cf898e088ea'
