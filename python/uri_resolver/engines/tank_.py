9965a5ddd6b66e9ef0395092746799f773f8048e
Benjamin Skinner <benjamin.skinner@student.uts.edu.au>
2018-05-03 10:46:08 +1000

    Moved authentication into the constructor, happens once

--- python/uri_resolver/engines/tank_.py
+++ python/uri_resolver/engines/tank_.py
@@ -32,6 +32,9 @@ class TankResolver(BaseResolver):
     def validate(self, uri):
         pass
 
+    def __init__(cls):
+        cls.authenticate()
+
     @classmethod
     def authenticate(self):
         # Import the ShotgunAuthenticator from the tank_vendor.shotgun_authentication
@@ -65,7 +68,6 @@ class TankResolver(BaseResolver):
         returns filepath: "/mnt/ala/mav/2018/jobs/s118/assets/setPiece/building01/model/model/caches/usd/building01_model_model_usd.v028.usd"
         """
 
-        cls.authenticate()
 
         print "uri resolver received: %s\n" % uri
 
@@ -145,7 +147,6 @@ class TankResolver(BaseResolver):
         returns uri: "tank:/maya_publish_asset_cache_usd?Step=model&Task=model&asset_type=setPiece&version=latest&Asset=building01"
         """
 
-        cls.authenticate()
 
         eng = sgtk.platform.current_engine()
         tk = eng.tank
@@ -176,7 +177,6 @@ class TankResolver(BaseResolver):
     @staticmethod
     def is_tank_asset(filepath, tk):
 
-        cls.authenticate()
 
         templ = tk.template_from_path(filepath)
         return True if templ else False
