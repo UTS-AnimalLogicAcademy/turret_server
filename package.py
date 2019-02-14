f4bef5d86450dede950140393f20ca2eb08afac6
Daniel Flood <daniel.flood-1@uts.edu.au>
2019-02-15 10:32:43 +1100

    moved resolver logic to separate repo

--- package.py
+++ package.py
@@ -2,17 +2,18 @@
 
 name = 'turret_server'
 
-version = '0.0.3'
+version = '0.0.4'
 
 authors = ['wen.tan',
            'ben.skinner',
            'daniel.flood']
 
-requires = ['pyzmq']
+requires = ['pyzmq',
+            'turret_resolver']
 
 build_requires = ['python']
 
 def commands():
     env.PATH.append('{root}/bin')
     env.TURRET_SERVER.set('{root}/bin/launch-turret-server.sh')
-    env.PYTHONPATH.append('{root}/python')
+
